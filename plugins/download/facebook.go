847e501b10e42945ec853e234c7de882432744c6
Takim <93022529+takimtod@users.noreply.github.com>
2024-01-31 09:25:34 +0800

    Update facebook.go

--- plugins/download/facebook.go
+++ plugins/download/facebook.go
@@ -3,10 +3,10 @@ package download
 import (
   "inc/lib"
   "fmt"
-  "net/http"
-   "encoding/json"
-  "io/ioutil"
-    "strings"
+  //"net/http"
+   //"encoding/json"
+  //"io/ioutil"
+  "strings"
 )
 
 func init() {
@@ -20,11 +20,29 @@ func init() {
     IsWaitt:  true,
     Exec: func(client *lib.Event, m *lib.IMessage) {
 
-        if !strings.Contains(m.Querry, "facebook") {
+      if !strings.Contains(m.Querry, "facebook") {
           m.Reply("Itu bukan link facebook")
         return
       }
-        
+
+      
+      resp, err := lib.Fb(m.Querry)
+      if err != nil {
+        fmt.Println("Error:", err)
+        return
+      }
+      
+      if resp != nil {
+        bytes, err := client.GetBytes(resp.HD)
+        if err != nil {
+          fmt.Println("Error:", err)
+          return
+        }
+        client.SendVideo(m.From, bytes, resp.Desc, m.ID)
+      }
+      
+      /*
+      
       type Media struct {
         URL        string `json:"url"`
         Quality    string `json:"quality"`
@@ -49,18 +67,20 @@ func init() {
 
 
       
-        resp, err := http.Get("https://skizo.tech/api/facebook?url="+ m.Querry +"&apikey=kertas")
+        resp, err := http.Get("https://skizo.tech/api/facebook?url="+ m.Querry +"&apikey=batu")
         if err != nil {
           fmt.Println("Error:", err)
           return
         }
         defer resp.Body.Close()
+      
 
         body, err := ioutil.ReadAll(resp.Body)
         if err != nil {
           fmt.Println("Error:", err)
           return
         }
+      
 
         var result Response
         err = json.Unmarshal(body, &result)
@@ -68,14 +88,15 @@ func init() {
           fmt.Println("Error:", err)
           return
         }
-
-      bytes, err := client.GetBytes(result.Medias[0].URL)
+      
+      
+      bytes, err := client.GetBytes(result.Medias[1].URL)
       if err != nil {
-        fmt.Println(err)
+        fmt.Println("Error:", err)
         return
       }
       client.SendVideo(m.From, bytes, " ", m.ID)
-      
+      */
     },
   })
 }
